ad04f18e9121530eb5e0b52a1d6c4d518a26b0d0
Brayhan De Aza <brayhandeaza@gmail.com>
2024-12-25 09:19:47 -0500

    sendNotification messages modified

--- src/expo/index.ts
+++ src/expo/index.ts
@@ -1,30 +1,27 @@
 import { evironmentVariables } from '@/constants';
-import { Expo, ExpoPushMessage } from 'expo-server-sdk';
+import { Expo, ExpoPushMessage, ExpoPushToken } from 'expo-server-sdk';
 
 const expo = new Expo({
     accessToken: evironmentVariables.EXPO_ACCESS_TOKEN,
     useFcmV1: true,
 });
 
-export const sendNotification = async (tokens: string[]) => {
+export const sendNotification = async (tokens: ExpoPushToken[]) => {
     try {
-        const messages: ExpoPushMessage[] = [];        
-        tokens.forEach(token => {
-            if (Expo.isExpoPushToken(token)) {
-                messages.push({
-                    to: token,
-                    sound: 'assets/audio/money.wav',
-                    body: 'This is a test notification',
-                    data: { withSome: 'data' },
-                })
+        const messages: ExpoPushMessage[] = tokens.map(token => {
+            return {
+                to: token,
+                sound: 'assets/audio/money.wav',
+                body: 'This is a test notification',
+                data: {
+                    withSome: 'data'
+                }
             }
-        })
+        });
 
-        const tickets = await expo.sendPushNotificationsAsync(messages);
-        console.log('Tickets: ', tickets);
+        await expo.sendPushNotificationsAsync(messages);
 
     } catch (error) {
         console.log(error);
-
     }
 }
