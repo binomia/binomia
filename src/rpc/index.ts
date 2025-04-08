4bccae013760ec5f9401944ebd12272c15d58a7e
Brayhan De Aza <brayhandeaza@gmail.com>
2025-04-07 23:57:26 -0400

    cryptografia

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -2,18 +2,17 @@ import { ZERO_ENCRYPTION_KEY } from "@/constants";
 import Email from "@/email";
 import { JSONRPCServer } from "json-rpc-2.0";
 import jwt from 'jsonwebtoken';
-import { Cryptography } from "@/helpers/cryptography";
 import { authServer } from "./clients";
 import { sendNotification } from "@/expo";
 import { Server } from "socket.io";
-
+import { HASH } from "cryptografia";
 
 export const initMethods = (server: JSONRPCServer, io: Server) => {
     server.addMethod("sendEmail", async ({ to, code, subject, text, html }: { to: string, code: string, subject: string, text: string, html: string }) => {
         try {
             console.log({ code });
 
-            const hash = await Cryptography.hash(code.toString());
+            const hash = await HASH.sha256Async(code.toString());
             const token = jwt.sign({ exp: 10 * 1000 * 60, data: hash }, ZERO_ENCRYPTION_KEY);
             const signature = await authServer("signData", { token })
 
