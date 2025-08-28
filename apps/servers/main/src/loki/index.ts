d3ac8307750ab7efe99251a55d2737a2b759cdba
Brayhan De Aza <brayhandeaza@gmail.com>
2025-08-28 08:57:51 -0400

    loki added

--- apps/servers/main/src/loki/index.ts
+++ apps/servers/main/src/loki/index.ts
@@ -1,14 +1,9 @@
+import { LOKI_PASSWORD, LOKI_URL, LOKI_USERNAME } from "@/constants";
 import axios from "axios";
 
-// Your Grafana Cloud Loki credentials
-const LOKI_USERNAME = "1148112"; // something like `123456`
-const LOKI_PASSWORD = "glc_eyJvIjoiMTM2NzA2MyIsIm4iOiJzdGFjay0xMTkwMzEzLWhsLXdyaXRlLWJpbm9taWEiLCJrIjoib1AyMzc4VjhISmEyNzk2c0ZWcXM2WHRZIiwibSI6eyJyIjoicHJvZC11cy1lYXN0LTAifX0=";
-const LOKI_URL = "https://logs-prod-036.grafana.net/loki/api/v1/push";
-// Replace <region> with your Grafana Cloud region, e.g. "us-central1"
 
-
-class Loki {
-    static push = async (message: string, labels: Record<string, string>) => {
+export class Loki {
+    static push = async (message: string, labels: Record<string, any>) => {
         try {
             const streams = [
                 {
