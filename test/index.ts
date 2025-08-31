ccad523a0d1673ea7bd71296f8d6f6138e959308
Brayhan De Aza <brayhandeaza@gmail.com>
2025-08-31 18:22:37 -0400

    sodium wrapp created

---
+++ test/index.ts
@@ -0,0 +1,14 @@
+// import { performance } from "perf_hooks"
+// import { AES, HASH, RSA } from 'cryptografia';
+
+// const ZERO_ENCRYPTION_KEY = "7335a5f1600b4d506acc41665bff99637df61d46b9a998d158b746a72539cbfd"
+// const message = "RnOjWE0GeyLw0N4TExwtmn91K1RHXrFSozbYxhPEnVeIm6X/Fc8wZz0nzpoBXuBkiPU/mxKPxsea2khwACRo3Fi88UZF18wiRYwfyQGbE8UyOdQwE69z/myvU2cPQHRH2ek6aWUAsgJIkgjWFWyDOXkHMk9EhS1sFQjwtBbkH4BDZxXmvNsQ+R09pAiifhOSUu8T6M/CUchc1eW2fJyu2PMVp/w8XlQQH6pujfzeZpdFzD8FLzyY1NN3Qlr1U0zDd4fqBg2W92cdRL66k3GKzw=="
+// const start = performance.now()
+
+// const decryptedPrivateKey = await AES.decrypt(message, ZERO_ENCRYPTION_KEY)
+
+// console.log(decryptedPrivateKey)
+
+
+// const end = performance.now()
+// console.log(end - start)
