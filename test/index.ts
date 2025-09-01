a6f9db1542682ce5359f82c059ddaffde4e24279
Brayhan De Aza <brayhandeaza@gmail.com>
2025-09-01 16:47:53 -0400

    cryptografia lib optimized

--- test/index.ts
+++ test/index.ts
@@ -5,7 +5,7 @@
 // const message = "RnOjWE0GeyLw0N4TExwtmn91K1RHXrFSozbYxhPEnVeIm6X/Fc8wZz0nzpoBXuBkiPU/mxKPxsea2khwACRo3Fi88UZF18wiRYwfyQGbE8UyOdQwE69z/myvU2cPQHRH2ek6aWUAsgJIkgjWFWyDOXkHMk9EhS1sFQjwtBbkH4BDZxXmvNsQ+R09pAiifhOSUu8T6M/CUchc1eW2fJyu2PMVp/w8XlQQH6pujfzeZpdFzD8FLzyY1NN3Qlr1U0zDd4fqBg2W92cdRL66k3GKzw=="
 // const start = performance.now()
 
-// const decryptedPrivateKey = await AES.decrypt(message, ZERO_ENCRYPTION_KEY)
+// const decryptedPrivateKey = await AES.decryptAsync(message, ZERO_ENCRYPTION_KEY)
 
 // console.log(decryptedPrivateKey)
 
